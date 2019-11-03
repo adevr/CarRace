f5894d6c7f4e289ea0ac6d98b2d2950a3dbc0689
adevr <alex.cst.reis@gmail.com>
2019-11-03 16:25:11 +0000

    Initial Classes & Structure

---
+++ headers/Bateria.h
@@ -0,0 +1,18 @@
+//
+// Created by Alex Reis on 02/11/2019.
+//
+
+#ifndef CARRACE_BATERIA_H
+#define CARRACE_BATERIA_H
+
+class Bateria{
+    int cap_max;
+    int cap_init;
+public:
+    Bateria()
+    ~Bateria();
+};
+
+
+#endif //CARRACE_Bateria_H
+
---
+++ headers/Carro.h
@@ -0,0 +1,18 @@
+//
+// Created by Alex Reis on 02/11/2019.
+//
+
+#ifndef CARRACE_CARRO_H
+#define CARRACE_CARRO_H
+
+
+class Carro {
+    string marca, modelo;
+    char id[1];
+    int dano;
+    int velocidade;
+
+};
+
+
+#endif //CARRACE_CARRO_H
