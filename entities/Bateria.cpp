7b4e3d41b7ed7879cc9d1ef49e5c39e93cb7ed2c
adevr <alex.cst.reis@gmail.com>
2019-11-07 23:05:01 +0000

    Final development Car Batery

--- entities/Bateria.cpp
+++ entities/Bateria.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "../headers/Bateria.h"
+
+void Bateria::setCapMax(int cap_max){ Bateria::cap_max = cap_max; }
+void Bateria::setCapInit(int cap_init){ Bateria::cap_init = cap_init; }
+void Bateria::setCapAtual(int cap_atual){ Bateria::cap_atual = cap_atual; }
+int Bateria::getCapAtual() { return Bateria::cap_atual; }
+
+void Bateria::carregaBateria(int quantidade){
+    int mah = Bateria::getCapAtual() + quantidade;
+
+    if(mah >= 100) Bateria::setCapAtual(100);
+    else Bateria::setCapAtual(mah);
+}
+
+void Bateria::descarregaBateria(int quantidade){
+    int mah = Bateria::getCapAtual() - quantidade;
+    
+    if(mah <= 0) Bateria::setCapAtual(0);
+    else Bateria::setCapAtual(mah);
+}
+
+Bateria::Bateria(int cap_max, int cap_init){
+    Bateria::setCapMax(cap_max);
+    Bateria::setCapInit(cap_init);
+    Bateria::setCapAtual(cap_init);
+}
+
+Bateria::~Bateria(){}
+
--- headers/Bateria.h
+++ headers/Bateria.h
@@ -8,14 +8,17 @@
 class Bateria{
     int cap_max;
     int cap_init;
+    int cap_atual;
 public:
     Bateria(int cap_max, int cap_init);
     ~Bateria();
     void carregaBateria(int quantidade = 0);
     void descarregaBateria(int quantidade = 0);
+    int getCapAtual();
 private:
     void setCapMax(int cap_max);
     void setCapInit(int cap_init);
+    void setCapAtual(int cap_atual);
 };
 
 
